ba619e7473a45cae3acfc531a7585da621997cf3
Muhammad Umar <muhammad.umar@sti2.at>
2022-05-11 14:57:37 +0200

    add routing

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
 
+// routes
+const accessibility = require("./routes/accessibility");
+const correctness = require("./routes/correctness");
+const completeness = require("./routes/completeness");
+
 app.get("/", (req, res) => {
-  res.send("Assessment");
+  res.send("complete result");
+});
+
+app.get("/accessibility", (req, res) => {
+  res.send("Accessibility");
+});
+app.get("/correctness", (req, res) => {
+  res.send("Correctness");
+});
+app.get("/completeness", (req, res) => {
+  res.send("Completeness");
 });
 
 app.listen(3000);
