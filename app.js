01bc9b7c88c4429d98d4a75cae7441826f4074dd
Muhammad Umar <muhammad.umar@sti2.at>
2022-05-15 18:24:06 +0200

    post req

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ const express = require("express");
 const app = express();
 const axios = require("axios");
 
+app.use(express.json());
+
 // routes
 const accessibility = require("./routes/accessibility");
 const correctness = require("./routes/correctness");
 const completeness = require("./routes/completeness");
+const insert = require("./routes/insert");
 
 app.get("/", (req, res) => {
   res.send("complete result");
 });
 
-app.post("/insert", (req, res) => {});
-
 app.use("/accessibility", accessibility);
-app.get("/correctness", correctness);
-app.get("/completeness", completeness);
+app.use("/correctness", correctness);
+app.use("/completeness", completeness);
+app.use("/insert", insert);
 
 app.listen(3000);
--- routes/insert.js
+++ routes/insert.js
@@ -1,73 +1,90 @@
-import SparqlClient from "sparql-http-client";
+// import SparqlClient from "sparql-http-client";
 
-const endpointUrl = "http://localhost:7200/statements";
-const updateUrl = endpointUrl;
-// const user = "ontocookie";
-// const password = "ontocookie123456";
+// const endpointUrl = "http://localhost:7200/statements";
+// const updateUrl = endpointUrl;
+// // const user = "ontocookie";
+// // const password = "ontocookie123456";
 
-const client = new SparqlClient({ endpointUrl, updateUrl, user, password });
+// const client = new SparqlClient({ endpointUrl, updateUrl, user, password });
 
-const annotate = (req, res) => {
-  const cookies = req.body;
+// const annotate = (req, res) => {
+//   const cookies = req.body;
 
-  var today = new Date();
-  var date =
-    today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
+//   var today = new Date();
+//   var date =
+//     today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
 
-  const insertUserQuery = `
-      PREFIX : <http://www.semanticweb.org/OntoCookie#>
-      PREFIX schema: <https://www.schema.org/>
+//   const insertUserQuery = `
+//       PREFIX : <http://www.semanticweb.org/OntoCookie#>
+//       PREFIX schema: <https://www.schema.org/>
 
-      INSERT DATA {:${cookies[0].userId} a <https://www.schema.org/Person>;
-            schema:identifier :${cookies[0].userId}.}
-      `;
+//       INSERT DATA {:${cookies[0].userId} a <https://www.schema.org/Person>;
+//             schema:identifier :${cookies[0].userId}.}
+//       `;
 
-  insertDataInKnowledgeGraph(insertUserQuery);
+//   insertDataInKnowledgeGraph(insertUserQuery);
 
-  for (let index = 0; index < cookies.length; index++) {
-    const element = cookies[index];
-    let element_domain = element.domain;
-    if (element_domain[0] === ".") {
-      element_domain = "";
-      for (let j = 1; j < element.domain.length; j++) {
-        element_domain += element.domain[j];
-      }
-    }
+//   for (let index = 0; index < cookies.length; index++) {
+//     const element = cookies[index];
+//     let element_domain = element.domain;
+//     if (element_domain[0] === ".") {
+//       element_domain = "";
+//       for (let j = 1; j < element.domain.length; j++) {
+//         element_domain += element.domain[j];
+//       }
+//     }
 
-    let cookieName = `Cookie_${element.userId}_${today.getTime()}_${index}`;
+//     let cookieName = `Cookie_${element.userId}_${today.getTime()}_${index}`;
 
-    let insertCookieQuery;
-    if (JSON.parse(element.session) == true) {
-      insertCookieQuery = `
-        PREFIX : <http://www.semanticweb.org/OntoCookie#>
-    
-        INSERT DATA {:${cookieName} a <http://www.semanticweb.org/OntoCookie#SessionCookie>;
-               :dateCreated :${date};
-               :hasDomain :${element_domain};
-               :name :${element.name} ;
-               :userId :${element.userId};.}
-        `;
-    } else {
-      insertCookieQuery = `
-        PREFIX : <http://www.semanticweb.org/OntoCookie#>
-    
-        INSERT DATA {:${cookieName} a <http://www.semanticweb.org/OntoCookie#Cookie>;
-               :dateCreated :${date};
-               :expires :${element.expirationDate};
-               :hasDomain :${element_domain};
-               :name :${element.name} ;
-               :userId :${element.userId};.}
-        `;
-    }
+//     let insertCookieQuery;
+//     if (JSON.parse(element.session) == true) {
+//       insertCookieQuery = `
+//         PREFIX : <http://www.semanticweb.org/OntoCookie#>
 
-    insertDataInKnowledgeGraph(insertCookieQuery);
-  }
+//         INSERT DATA {:${cookieName} a <http://www.semanticweb.org/OntoCookie#SessionCookie>;
+//                :dateCreated :${date};
+//                :hasDomain :${element_domain};
+//                :name :${element.name} ;
+//                :userId :${element.userId};.}
+//         `;
+//     } else {
+//       insertCookieQuery = `
+//         PREFIX : <http://www.semanticweb.org/OntoCookie#>
 
-  res.status(200).json({ message: "Cookies inserted successfully!" });
-};
+//         INSERT DATA {:${cookieName} a <http://www.semanticweb.org/OntoCookie#Cookie>;
+//                :dateCreated :${date};
+//                :expires :${element.expirationDate};
+//                :hasDomain :${element_domain};
+//                :name :${element.name} ;
+//                :userId :${element.userId};.}
+//         `;
+//     }
 
-async function insertDataInKnowledgeGraph(insertCookieQuery) {
-  const stream = await client.query.update(insertCookieQuery);
-}
+//     insertDataInKnowledgeGraph(insertCookieQuery);
+//   }
 
-export { annotate };
+//   res.status(200).json({ message: "Cookies inserted successfully!" });
+// };
+
+// async function insertDataInKnowledgeGraph(insertCookieQuery) {
+//   const stream = await client.query.update(insertCookieQuery);
+// }
+
+// export { annotate };
+
+const express = require("express");
+const router = express.Router();
+
+// app.use(express.json());
+// router.use((req, res, next) => {
+//   console.log("Time: ", Date.now());
+//   next();
+// });
+
+router.post("/", (req, res) => {
+  const wifi = req.body;
+  const names = Object.keys(wifi);
+  console.log(names);
+});
+
+module.exports = router;
